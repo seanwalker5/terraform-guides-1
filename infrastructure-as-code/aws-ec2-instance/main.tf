5268ace28ab3095829e92330a62e4569099a7549
Sean Walker <45837093+seanwalker5@users.noreply.github.com>
2020-03-09 08:57:26 -0400

    Update main.tf
    Update for 0.12

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -1,21 +1,21 @@
 terraform {
-  required_version = ">= 0.11.0"
+  required_version = ">= 0.12.0"
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.aws_region
 
   
   tags = {
-    Name  = "${var.name}"
-    TTL   = "${var.ttl}"
-    Owner = "${var.owner}"
+    Name  = var.name
+    TTL   = var.ttl
+    Owner = var.owner
     Description = "This is a demo instance"
     
     }
