0177fda1c33fb9ecb39fe09472c6c54cb7491de2
Sean Walker <45837093+seanwalker5@users.noreply.github.com>
2020-09-15 14:37:36 -0400

    Update variables.tf

--- infrastructure-as-code/aws-ec2-instance/variables.tf
+++ infrastructure-as-code/aws-ec2-instance/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = ""
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default = ""
 }
 
 variable "instance_type" {
